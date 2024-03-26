ff1529562a184978983fb960d4eb2f894615b096
jpmoncao <pedrohjoao44@gmail.com>
2024-03-26 20:37:32 -0300

    feat: finish meeting animate

--- portfolio-2024/src/components/Meeting/Meeting.jsx
+++ portfolio-2024/src/components/Meeting/Meeting.jsx
@@ -11,15 +11,20 @@ export default function Meeting() {
         width: '100%'
     }
 
+    const iconStyle = {
+
+    }
+
     const variants = {
-        rightToLeftInitial: { x: 25, opacity: 0}, 
-        rightToLeftAnimate: { x: 0, opacity: 1}, 
-        LeftToRightInitial: { x: -25, opacity: 0}, 
-        LeftToRightAnimate: { x: 0, opacity: 1}, 
-        DownToTopInitial: { y: 25, opacity: 0}, 
-        DownToTopAnimate: { y: 0, opacity: 1}, 
-        FadeInitial: { opacity: 0}, 
-        FadeAnimate: { opacity: 1}, 
+        rightToLeftInitial: { x: 75, opacity: 0 },
+        rightToLeftAnimate: { x: 0, opacity: 1 },
+        LeftToRightInitial: { x: -75, opacity: 0 },
+        LeftToRightAnimate: { x: 0, opacity: 1 },
+        DownToTopInitial: { y: 75, opacity: 0 },
+        DownToTopAnimate: { y: 0, opacity: 1 },
+        FadeInitial: { opacity: 0 },
+        FadeAnimate: { opacity: 1 },
+        scaleUp: { size: 1.5 }
     }
 
     return (
@@ -41,44 +46,98 @@ export default function Meeting() {
                     transition={{ duration: 1.5, delay: 0.5 }}
                 >FullStack Web Developer</motion.h2>
             </article>
-            <article className='circles'>
-                <motion.img 
-                    src="https://github.com/jpmoncao.png" 
-                    alt="Foto de perfil de João Pedro" 
-                    initial={variants.rightToLeftInitial}
-                    animate={variants.rightToLeftAnimate}
-                    transition={{ duration: 0.5, delay: 0.25 }}
-                    style={{   
-                        ...circleStyle,
-                        zIndex: 4,
-                        backgroundColor: 'var(--mint)',
-                        boxShadow: 'inset 0px 0px 4px var(--mint)',
-                        left: '10vw',
-                    }}
-                />
-                <motion.div
-                    initial={variants.rightToLeftInitial}
-                    animate={variants.rightToLeftAnimate}
-                    transition={{ duration: 0.5, delay: 0.5 }}
-                    style={{   
-                        ...circleStyle,
-                        zIndex: 3,
-                        backgroundColor: 'var(--persiangreen)',
-                        boxShadow: 'inset 0px 0px 4px var(--persiangreen)',
-                        left: '5vw'
-                    }}
-                />
-                <motion.div
-                    initial={variants.rightToLeftInitial}
-                    animate={variants.rightToLeftAnimate}
-                    transition={{ duration: 0.5, delay: 0.75 }}
-                    style={{ 
-                        ...circleStyle,
-                        zIndex: 2,
-                        backgroundColor: 'var(--aquamarine)',
-                        boxShadow: 'inset 0px 0px 4px var(--aquamarine)',
-                    }}
-                />
+            <article className='circles-container'>
+                <article className='social-icons'>
+                    <motion.a
+                        href='https://www.linkedin.com/in/jpmoncao'
+                        target='_blank'
+                        initial={variants.DownToTopInitial}
+                        animate={variants.DownToTopAnimate}
+                        transition={{ duration: 0.5, delay: 0.45 }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: {
+                                duration: 0,
+                                delay: 0
+                            }
+                        }}
+                        style={{ marginTop: '-0.5rem' }}
+                    >
+                        <img src='icons/linkedin.svg' />
+                    </motion.a>
+                    <motion.a
+                        href='https://github.com/jpmoncao'
+                        target='_blank'
+                        initial={variants.DownToTopInitial}
+                        animate={variants.DownToTopAnimate}
+                        transition={{ duration: 0.5, delay: 0.6 }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: {
+                                duration: 0,
+                                delay: 0
+                            }
+                        }}
+                    >
+                        <img src='icons/github.svg' />
+                    </motion.a>
+                    <motion.a
+                        href='/docs/cv.pdf'
+                        target='_blank'
+                        // download={true}
+                        initial={variants.DownToTopInitial}
+                        animate={variants.DownToTopAnimate}
+                        transition={{ duration: 0.5, delay: 0.75 }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: {
+                                duration: 0,
+                                delay: 0
+                            }
+                        }}
+                    >
+                        <img src='icons/curriculum.svg' />
+                    </motion.a>
+                </article>
+                <article className='circles'>
+                    <motion.img
+                        src="https://github.com/jpmoncao.png"
+                        alt="Foto de perfil de João Pedro"
+                        initial={variants.rightToLeftInitial}
+                        animate={variants.rightToLeftAnimate}
+                        transition={{ duration: 0.5, delay: 0.5 }}
+                        style={{
+                            ...circleStyle,
+                            zIndex: 4,
+                            backgroundColor: 'var(--mint)',
+                            boxShadow: 'inset 0px 0px 4px var(--mint)',
+                            left: '10vw',
+                        }}
+                    />
+                    <motion.div
+                        initial={variants.rightToLeftInitial}
+                        animate={variants.rightToLeftAnimate}
+                        transition={{ duration: 0.5, delay: 0.75 }}
+                        style={{
+                            ...circleStyle,
+                            zIndex: 3,
+                            backgroundColor: 'var(--persiangreen)',
+                            boxShadow: 'inset 0px 0px 4px var(--persiangreen)',
+                            left: '5vw'
+                        }}
+                    />
+                    <motion.div
+                        initial={variants.rightToLeftInitial}
+                        animate={variants.rightToLeftAnimate}
+                        transition={{ duration: 0.5, delay: 1 }}
+                        style={{
+                            ...circleStyle,
+                            zIndex: 2,
+                            backgroundColor: 'var(--aquamarine)',
+                            boxShadow: 'inset 0px 0px 4px var(--aquamarine)',
+                        }}
+                    />
+                </article>
             </article>
         </section>
     )
