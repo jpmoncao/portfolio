ff1db5fda0a2821e9e2527e6feaa81f2ce430fc0
jpmoncao <pedrohjoao44@gmail.com>
2024-06-20 12:43:43 -0300

    feat: concluindo a seção de informações

--- portfolio-2024/src/App.jsx
+++ portfolio-2024/src/App.jsx
@@ -1,11 +1,13 @@
 import Meeting from './components/Meeting/Meeting'
 import Info from './components/Info/Info'
+import Skills from './components/Skills/Skills'
 
 function App() {
   return (
     <>
       <Meeting />
       <Info />
+      <Skills />
     </>
   )
 }
---
+++ portfolio-2024/src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Skills() {
+
+    return (
+        <section className='Info' style={{ background: "#2C363F" }}>
+            <article className='text'>
+                <h1>{'<skills />'}</h1>
+            </article>
+        </section>
+    )
+}
