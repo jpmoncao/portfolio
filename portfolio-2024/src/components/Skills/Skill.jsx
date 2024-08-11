85b667508d47115a1b6ab0baa765e486bed40693
jpmoncao <pedrohjoao44@gmail.com>
2024-08-11 15:24:56 -0300

    in progress: cards de habilidades técnicas

--- portfolio-2024/src/components/Skills/Skill.jsx
+++ portfolio-2024/src/components/Skills/Skill.jsx
@@ -1,11 +1,17 @@
-export default function Skill(props) {
+export default function Skill(props, key) {
     const activeStars = [0, 1, 2, 3, 4]
 
     return (
-        <article className="Skill">
-            <img className="skill-img" src={props.imgContent} alt={`Logo da tecnologia ${props.name}`} />
-            <span style={{ display: 'none' }}>
-                <h2>{props.name}</h2>
+        <article className="Skill" htmlFor={`switch-${props.name ?? ''}`}>
+            <div className="skill-front skill-card">
+                <img className="skill-img" src={props.imgContent} alt={`Logo da tecnologia ${props.name}`} />
+            </div>
+            <div className="skill-back skill-card">
+                <div className="skill-card-title">
+                    <img className="skill-img-xs" src={props.imgContent} alt={`Logo da tecnologia ${props.name}`} />
+                    <h2>{props.name}</h2>
+                </div>
+                <p>{props.description}</p>
                 <div>
                     {activeStars.map((value, index) => {
                         return <span key={index}>
@@ -17,7 +23,7 @@ export default function Skill(props) {
                         </span>
                     })}
                 </div>
-            </span>
+            </div>
         </article>
     )
 }
