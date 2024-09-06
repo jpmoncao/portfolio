8898b1a5d47b58923d1aac29c5ff51376506f483
jpmoncao <pedrohjoao44@gmail.com>
2024-09-05 22:17:40 -0300

    feat: envio de email com toaster de notificação

--- portfolio-2024/src/components/Contact/Contact.jsx
+++ portfolio-2024/src/components/Contact/Contact.jsx
@@ -1,31 +1,5 @@
 import './styles.css';
-import emailjs from 'emailjs-com';
-
-// const userID = process.env.REACT_APP_USER_ID;
-// const templateID = process.env.REACT_APP_TEMPLATE_ID;
-// const serviceID = process.env.REACT_APP_SERVICE_ID;
-
-const userID = '_9N8E1ywTFsrFbJ2P'
-const templateID = 'template_jush1m5'
-const serviceID = 'service_xe5urbr'
-
-function sendEmail(e) {
-    e.preventDefault();
-
-    emailjs.sendForm(
-        serviceID,
-        templateID,
-        e.target,
-        userID
-    )
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    e.target.reset();
-}
-
+import Form from './Form';
 
 export default function Contact() {
     return (
@@ -49,14 +23,9 @@ export default function Contact() {
                     </article>
                 </aside>
 
-                <form onSubmit={sendEmail}>
-                    <input type="text" name="name" id="name" placeholder='Your name' />
-                    <input type="email" name="email" id="email" placeholder='Your email' />
-                    <textarea name="message" id="message" placeholder='Type your message...'></textarea>
-                    <button type='submit'>Send</button>
-                </form>
+                <Form />
             </section>
-            <footer>dev by <a href="https://github.com/jpmoncao">João Pedro Monção</a></footer>
+            <footer>dev by <a target="_blank" href="https://github.com/jpmoncao">João Pedro Monção</a></footer>
         </section>
     );
 }
---
+++ portfolio-2024/src/components/Contact/Form.jsx
@@ -0,0 +1,71 @@
+import './styles.css';
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
+
+const userID = import.meta.env.VITE_USER_ID;
+const templateID = import.meta.env.VITE_TEMPLATE_ID;
+const serviceID = import.meta.env.VITE_SERVICE_ID;
+const reply_to = import.meta.env.VITE_REPLY_TO;
+
+export default function Form() {
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const name = e.target.querySelector('#from_name').value;
+        const email = e.target.querySelector('#from_email').value;
+        const message = e.target.querySelector('#message').value;
+
+        if (name.trim() === '') {
+            toast.error('O nome é um campo obrigatório');
+            return;
+        }
+
+        if (email.trim() === '') {
+            toast.error('O email é um campo obrigatório');
+            return;
+        }
+
+        if (message.trim() === '') {
+            toast.error('A mensagem é um campo obrigatório');
+            return;
+        }
+
+        toast.promise(
+            emailjs.sendForm(
+                serviceID,
+                templateID,
+                e.target,
+                userID
+            ),
+            {
+                loading: 'Enviando...',
+                success: 'E-mail enviado com sucesso!',
+                error: 'Falha ao enviar o e-mail!',
+            }
+        );
+
+        e.target.reset();
+    }
+
+
+    return (
+        <form onSubmit={sendEmail}>
+            <div>
+                <Toaster
+                    position="top-center"
+                    reverseOrder={true}
+                    style={{
+                        background: '#333',
+                        color: '#fff',
+                    }}
+                />
+            </div>
+            <input type="text" name="from_name" id="from_name" placeholder='Your name' />
+            <input type="email" name="from_email" id="from_email" placeholder='Your email' />
+            <input type="hidden" name="reply_to" id="reply_to" value={reply_to} />
+            <textarea name="message" id="message" placeholder='Type your message...'></textarea>
+            <button type='submit'>Send</button>
+        </form>
+    )
+}
